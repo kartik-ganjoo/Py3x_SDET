f26d31f22703a0751d1a962ddca6871e74477dcf
kartik2711 <kartik.ganjoo@gmail.com>
2024-06-15 18:19:35 -0700

    assignment 1, Q2 added

---
+++ Assignment_1/Q2.py
@@ -0,0 +1,13 @@
+# ==============================================
+#   Q2:
+#       Calculate square and cube of a number
+#       - input -> float
+#       - output -> float
+# ==============================================
+
+n = float(input("Enter a number > "))
+
+square = n ** 2
+cube = n ** 3
+
+print(f"Num = {n}\nSquare = {square}\nCube = {cube}")
